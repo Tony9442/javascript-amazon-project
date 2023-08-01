74a54d705a337146696afbd1de600d559bd10df2
Chidera Anthony <geanthony94@gmail.com>
2023-08-01 13:15:15 -0700

    add file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -36,6 +36,22 @@ const product = [{
     count: 56
   },
   priceCent: 799
+}, {
+     image: "images/products/black-2-slot-toaster.jpg",
+     name: '2 Slot Toaster - Black',
+     rating: {
+      stars: 5,
+      count: 2197
+     },
+     priceCent: 1899
+}, {
+  image: "images/products/6-piece-white-dinner-plate-set.jpg",
+  name: '6 Piece White Dinner Plate Set',
+  rating: {
+    stars: 4,
+    count: 37
+  },
+  priceCent: 2065
 }];
 
 let productHTML ='';
@@ -96,3 +112,7 @@ product.forEach((products) => {
 });
 
  document.querySelector('.js-products-grid').innerHTML =  productHTML;
+
+
+ //BENEFIT OF GENERATING HTML
+
