93d3ae3658fb3f0c16ec88d00f153c0a07b2f3e6
Chidera Anthony <geanthony94@gmail.com>
2023-08-01 13:26:37 -0700

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -8,52 +8,6 @@
 
 // AND USE OBJECT TO REPRESENT EACH PRODUCT LIKE Image, name, and a price BECAUSE OBJETC LETS US GROUP MULTIPLE VALUES TOGETHER
 
-
-const product = [{
-
-   image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-   name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-   rating: {
-    stars: 4.5,
-    count: 87
-   },
-   priceCent: 1090
-
-}, {
-  image:
-  'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCent: 2095
-}, {
-  image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCent: 799
-}, {
-     image: "images/products/black-2-slot-toaster.jpg",
-     name: '2 Slot Toaster - Black',
-     rating: {
-      stars: 5,
-      count: 2197
-     },
-     priceCent: 1899
-}, {
-  image: "images/products/6-piece-white-dinner-plate-set.jpg",
-  name: '6 Piece White Dinner Plate Set',
-  rating: {
-    stars: 4,
-    count: 37
-  },
-  priceCent: 2065
-}];
-
 let productHTML ='';
 
 product.forEach((products) => {
